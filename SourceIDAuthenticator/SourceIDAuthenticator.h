b97ed0e99446434c7f519fce0146b04d339b91b3
Oko-Osi Korede <korede.oko-osi@fcmb.com>
2025-05-04 15:18:53 +0100

    Initial Commit

---
+++ SourceIDAuthenticator/SourceIDAuthenticator.h
@@ -0,0 +1,18 @@
+//
+//  SourceIDAuthenticator.h
+//  SourceIDAuthenticator
+//
+//  Created by Oko-osi Korede Ibrahim on 04/05/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SourceIDAuthenticator.
+FOUNDATION_EXPORT double SourceIDAuthenticatorVersionNumber;
+
+//! Project version string for SourceIDAuthenticator.
+FOUNDATION_EXPORT const unsigned char SourceIDAuthenticatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SourceIDAuthenticator/PublicHeader.h>
+
+
